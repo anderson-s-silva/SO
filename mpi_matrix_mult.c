aafa607a0fc3029a9d130acc4604a956414f21e4
Anderson S. Silva <andsoares2001@gmail.com>
2022-04-11 12:05:46 -0300

    Update mpi_matrix_mult.c

--- mpi_matrix_mult.c
+++ mpi_matrix_mult.c
@@ -1,3 +1,9 @@
+/**
+* @file mpi_matrix_mult.c
+* @author Anderson Soares da Silva
+* @date 11 abr 2022
+* @brief  exercise mpi matrizes
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -37,7 +43,7 @@ int main(int argc, char **argv){
 	int id, nprocess;
 	int linhas;
 	int mtype;
-	int destino, src, deslocA, deslocB;
+	int destino, src, deslocI, deslocJ;
 	double start_time, end_time;
 	int i, j, k, l;
 
@@ -62,32 +68,32 @@ int main(int argc, char **argv){
 					c[i][j] = c[i][j] + a[i][k]*b[k][j];
 				} //fim for i
 			}  //fim for j
-			end_time = MPI_Wtime();
+			end_time = MPI_Wtime();//pausa tempo de exec
 			print_matriz();//---------------------------------
 			printf("Numero de processos: %2d Tempo: %f\n", nprocess, end_time-start_time);
 		} // end  if(nprocess == 1)
 		else {
 			for(l = 0; l < nprocess; l++){
-				deslocB = linhas*l;  //comeca do (tamanho do bloco * id do processo)
-				deslocA = linhas;
+				deslocJ = linhas*l;  //comeca do (tamanho do bloco * id do processo)
+				deslocI = linhas;
 				mtype = FROM_MASTER; // tag =1
 
 				for(destino = 1; destino < nprocess; destino++){
-					MPI_Send(&deslocA, 1, MPI_INT, destino, mtype, MPI_COMM_WORLD);
-					MPI_Send(&deslocB, 1, MPI_INT, destino, mtype, MPI_COMM_WORLD);
+					MPI_Send(&deslocI, 1, MPI_INT, destino, mtype, MPI_COMM_WORLD);
+					MPI_Send(&deslocJ, 1, MPI_INT, destino, mtype, MPI_COMM_WORLD);
 					MPI_Send(&linhas, 1, MPI_INT, destino, mtype, MPI_COMM_WORLD);
-					MPI_Send(&a[deslocA][0], linhas*SIZE, MPI_DOUBLE, destino, mtype, MPI_COMM_WORLD);
-					MPI_Send(&b[0][deslocB], linhas*SIZE, MPI_DOUBLE, destino, mtype, MPI_COMM_WORLD);
+					MPI_Send(&a[deslocI][0], linhas*SIZE, MPI_DOUBLE, destino, mtype, MPI_COMM_WORLD);
+					MPI_Send(&b[0][deslocJ], linhas*SIZE, MPI_DOUBLE, destino, mtype, MPI_COMM_WORLD);
 
-					deslocA += linhas;
-					deslocB = (deslocB+linhas)%SIZE;
+					deslocI += linhas;
+					deslocJ = (deslocJ+linhas)%SIZE;
 				} // fim for destino
-				deslocA = linhas;
-				deslocB = linhas*l;
+				deslocI = linhas;
+				deslocJ = linhas*l;
 
 				//--mult o local final e imprime o mult final global
-				for(i = 0; i < deslocA; i++) {
-					for(j = deslocB; j < deslocB+linhas; j++) {
+				for(i = 0; i < deslocI; i++) {
+					for(j = deslocJ; j < deslocJ+linhas; j++) {
 						for(k = 0; k < SIZE; k++){
 							c[i][j] = c[i][j] + a[i][k]*b[k][j];
 						}//fim for k
@@ -96,11 +102,11 @@ int main(int argc, char **argv){
 				/*- espera os resultados de todas as tarefas do trabalhador */
 				mtype = FROM_WORKER;
 				for(src = 1; src < nprocess; src++){
-					MPI_Recv(&deslocA, 1, MPI_INT, src, mtype, MPI_COMM_WORLD, &status);
-					MPI_Recv(&deslocB, 1, MPI_INT, src, mtype, MPI_COMM_WORLD, &status);
+					MPI_Recv(&deslocI, 1, MPI_INT, src, mtype, MPI_COMM_WORLD, &status);
+					MPI_Recv(&deslocJ, 1, MPI_INT, src, mtype, MPI_COMM_WORLD, &status);
 					MPI_Recv(&linhas, 1, MPI_INT, src, mtype, MPI_COMM_WORLD, &status);
 					for(i = 0; i < linhas; i++) {
-						MPI_Recv(&c[deslocA+i][deslocB], deslocA, MPI_DOUBLE, src, mtype, MPI_COMM_WORLD, &status);
+						MPI_Recv(&c[deslocI+i][deslocJ], deslocI, MPI_DOUBLE, src, mtype, MPI_COMM_WORLD, &status);
 					} //fim for scr
 				}//fim for i
 			} //fim for l
@@ -115,14 +121,14 @@ int main(int argc, char **argv){
 		if(nprocess > 1) {
 			for(l = 0; l < nprocess; l++){
 				mtype = FROM_MASTER;
-				MPI_Recv(&deslocA, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD,&status);
-				MPI_Recv(&deslocB, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD,&status);
+				MPI_Recv(&deslocI, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD,&status);
+				MPI_Recv(&deslocJ, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD,&status);
 				MPI_Recv(&linhas, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD,&status);
-				MPI_Recv(&a[deslocA][0], linhas*SIZE, MPI_DOUBLE, 0, mtype,	MPI_COMM_WORLD, &status);
-				MPI_Recv(&b[0][deslocB], linhas*SIZE, MPI_DOUBLE, 0, mtype,	MPI_COMM_WORLD, &status);
+				MPI_Recv(&a[deslocI][0], linhas*SIZE, MPI_DOUBLE, 0, mtype,	MPI_COMM_WORLD, &status);
+				MPI_Recv(&b[0][deslocJ], linhas*SIZE, MPI_DOUBLE, 0, mtype,	MPI_COMM_WORLD, &status);
 
-				for(i = deslocA; i < deslocA+linhas; i++) {
-					for(j = deslocB; j < deslocB+linhas; j++) {
+				for(i = deslocI; i < deslocI+linhas; i++) {
+					for(j = deslocJ; j < deslocJ+linhas; j++) {
 						for(k = 0; k < SIZE; k++){
 							c[i][j] = c[i][j] + a[i][k]*b[k][j];
 						} //fim for j
@@ -130,11 +136,11 @@ int main(int argc, char **argv){
 				} //fim for l
 
 				mtype = FROM_WORKER;
-				MPI_Send(&deslocA, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD);
-				MPI_Send(&deslocB, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD);
+				MPI_Send(&deslocI, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD);
+				MPI_Send(&deslocJ, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD);
 				MPI_Send(&linhas, 1, MPI_INT, 0, mtype, MPI_COMM_WORLD);
 				for(i = 0; i < linhas; i++){
-					MPI_Send(&c[deslocA+i][deslocB], deslocA, MPI_DOUBLE, 0, mtype, MPI_COMM_WORLD);
+					MPI_Send(&c[deslocI+i][deslocJ], deslocI, MPI_DOUBLE, 0, mtype, MPI_COMM_WORLD);
 				} //fim for i
 			}//fim for l
 		} //fim if (nprocess > 1)
